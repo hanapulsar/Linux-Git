5a1120fa8a0853ddb17eccb1220f1cdd9a058f40
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2025-10-23 00:25:14 +0400

    Add solution to Lab-1

---
+++ lab-1/second.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+mkdir -pv analysis
+
+cat faculty.csv | grep -oiE [^,]*$ | grep -oE [^+]+ | sort | uniq -c | sort -nr |
+while read count name
+do
+if [ "$count" -gt 10 ]
+then
+echo "$name - $count"
+fi
+done > analysis/big_groups.txt
+
+echo "Name, Position" > analysis/ai_cs_join.csv
+cat faculty.csv | grep -i "Artificial Intelligence" | grep -i "Computer Systems" | sed -E 's/^([^,]+,[^,]+),.*/\1/' >> analysis/ai_cs_join.csv
+
+cat faculty.csv | sed 's/,.*//' | sort | nl > analysis/sorted_names.txt
