facc0693f0cb042d991e6ac9fe51987edb89c103
hanapulsar <217953127+hanapulsar@users.noreply.github.com>
2025-10-24 01:20:26 +0400

    fix script: remove unnecessary first line with names in csv; trim whitespace from research areas

--- lab-1/second.sh
+++ lab-1/second.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir -pv analysis
 
-cat faculty.csv | grep -oiE [^,]*$ | grep -oE [^+]+ | sort | uniq -c | sort -nr |
+cat faculty.csv | grep -oiE [^,]*$ | grep -oE [^+]+ | sed -E 's/^[ \t]*//;s/[ \t]*$//' | sort | uniq -c | sort -nr |
 while read count name
 do
 if [ "$count" -gt 10 ]
@@ -10,7 +10,7 @@ echo "$name - $count"
 fi
 done > analysis/big_groups.txt
 
-echo "Name, Position" > analysis/ai_cs_join.csv
+#echo "Name, Position" > analysis/ai_cs_join.csv
 cat faculty.csv | grep -i "Artificial Intelligence" | grep -i "Computer Systems" | sed -E 's/^([^,]+,[^,]+),.*/\1/' >> analysis/ai_cs_join.csv
 
 cat faculty.csv | sed 's/,.*//' | sort | nl > analysis/sorted_names.txt
